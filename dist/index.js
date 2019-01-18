b0d7ccdb3c5f86c47a6762c58f5dda9b577c078b
Dani <daniel.anido17@gmail.com>
2019-01-18 16:27:25 +0100

    Minnor changes

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import DateTimePicker from 'material-ui-datetimepicker';
-import DatePickerDialog from 'material-ui/DatePicker/DatePickerDialog'
-import TimePickerDialog from 'material-ui/TimePicker/TimePickerDialog';
+import { View,Text } from 'react-native';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from 'material-ui-pickers';
+import { TimePicker } from 'material-ui-pickers';
+import { DatePicker } from 'material-ui-pickers';
+import { DateTimePicker } from 'material-ui-pickers';
 
 class DateTimePickerWeb extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dateTime : null,
+      selectedDate: new Date()
+    };
   }
+
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+  };
+
+  setDate = (dateTime) => this.setState({ dateTime })
 
   render() {
     const { style } = this.props;
 
     return (
-      <View style={style}>
+      <View style={{width : "100%", height : "100%", position : "fixed", backgroundColor : "red"}}>
         <Text>Modal para el DateTimePicker</Text>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <DatePicker value={selectedDate} onChange={this.handleDateChange} />
+          <TimePicker value={selectedDate} onChange={this.handleDateChange} />
+          <DateTimePicker value={selectedDate} onChange={this.handleDateChange} />
+        </MuiPickersUtilsProvider>
       </View>
     );
   }
